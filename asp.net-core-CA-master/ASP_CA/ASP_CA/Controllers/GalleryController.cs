f5ac895ecdb054af06ce43d24517f182a23fc9de
Jiayuan-Hu <Jiayuan-Hu-email@example.com>
2020-11-18 20:26:01 +0800

    Update

---
+++ asp.net-core-CA-master/ASP_CA/ASP_CA/Controllers/GalleryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ASP_CA.Models;
+using ASP_CA.Data;
+
+namespace ASP_CA.Controllers
+{
+    public class GalleryController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            List<Product> products = ProductData.GetAllProducts();
+
+            ViewData["products"] = products;
+
+            ViewData["header"] = "on";
+            ViewData["sessionId"] = HttpContext.Request.Cookies["sessionId"];
+            return View();
+        }
+
+        public IActionResult Click()
+        {
+
+            ViewData["products"] = null;
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
